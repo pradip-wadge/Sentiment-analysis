2d2e981ff51740debc00498f4fa4a10ca66f3f76
Pradip Wadage <111996538+pradip-wadge@users.noreply.github.com>
2024-11-25 19:05:10 +0530

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,11 @@
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "YOUR_SECRET_KEY"
+ACCESS_TOKEN_SECRET = "YOUR_SECRET_KEY"
+CONSUMER_KEY = "YOUR_SECRET_KEY"
+CONSUMER_SECRET = "YOUR_SECRET_KEY"
+
+#cfQ6DydPpqpGJRqcDnTvRLYmj
+#q1emnpgA6AjbQm7kxiK447V4uwt9YiJCLjAvdzBbhUztwLKWCN
+#barrer token: AAAAAAAAAAAAAAAAAAAAAAummwEAAAAAjOKxm0ivjeTd8%2Bpn2b1PwZG%2Bcag%3DK79TAcqQEhTBdAVgRsfswlUrIbb3xnQbieNRmCOwdoGKCy1PjH
+#Client secret: M2cG8rVx9IsdqwcTwhd1rFpre6yhPEGy4tId5Qg6xTMtSETgv-
+#M2cG8rVx9IsdqwcTwhd1rFpre6yhPEGy4tId5Qg6xTMtSETgv-
